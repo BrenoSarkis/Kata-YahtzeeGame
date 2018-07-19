305eb763a4285afa0247c4733df4725625605d7f
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-19 20:34:50 +0200

    PlayerRolls_Ones_CalculatesCorrectScore

--- Kata.YahtzeeGame/Kata.YahtzeeGame/Class1.cs
+++ Kata.YahtzeeGame/Kata.YahtzeeGame/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using NUnit.Framework;
 
 namespace Kata.YahtzeeGame
@@ -11,12 +12,50 @@ namespace Kata.YahtzeeGame
         {
             var yahtzee = new YahtzeeGame();
 
-            Assert.That(yahtzee.PlayerScore, Is.EqualTo(0));
+            Assert.That(yahtzee.PlayerScore(new int[0]), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void PlayerRolls_Ones_CalculatesCorrectScore()
+        {
+            var fakeDice = new FakeDice(setAllRollsTo: 1);
+            var player = new Player(fakeDice);
+            var yahtzee = new YahtzeeGame();
+
+            var rolls = player.Roll();
+
+            Assert.That(yahtzee.PlayerScore(rolls), Is.EqualTo(5));
+        }
+    }
+
+    public class FakeDice
+    {
+        public FakeDice(int setAllRollsTo)
+        {
+            Value = setAllRollsTo;
+        }
+        public int Value { get; private set; }
+    }
+
+    public class Player
+    {
+        private readonly FakeDice fakeDice;
+
+        public Player(FakeDice fakeDice)
+        {
+            this.fakeDice = fakeDice;
+        }
+        public int[] Roll()
+        {
+            return new[] {1, 1, 1, 1, 1};
         }
     }
 
     public class YahtzeeGame
     {
-        public int PlayerScore { get; } = 0;
+        public int PlayerScore(int[] rolls)
+        {
+            return rolls.Sum();
+        }
     }
 }
