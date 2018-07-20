25a41cc0092a0394c2dc52cf28bbf5003c31c187
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-20 20:33:08 +0200

    PlayerRolls_OnlyTheDesiredValueIsConsideredOnScore

--- Kata.YahtzeeGame/Kata.YahtzeeGame/YahtzeeTests.cs
+++ Kata.YahtzeeGame/Kata.YahtzeeGame/YahtzeeTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace Kata.YahtzeeGame
 {
@@ -13,7 +15,7 @@ namespace Kata.YahtzeeGame
         {
             var yahtzee = new YahtzeeGame();
 
-            Assert.That(yahtzee.PlayerScore(new int[0]), Is.EqualTo(0));
+            Assert.That(yahtzee.CalculateScore(Category.Chance, 0), Is.EqualTo(0));
         }
 
         [TestCase(1, 5)]
@@ -21,48 +23,79 @@ namespace Kata.YahtzeeGame
         [TestCase(3, 15)]
         [TestCase(4, 20)]
         [TestCase(5, 25)]
+        [TestCase(6, 30)]
         public void PlayerRolls_TheSameValueForEveryRow_CalculatesCorrectScore(int rollValue, int expectedScore)
         {
-            var fakeDice = new FakeDice(setAllRollsTo: rollValue);
-            var player = new Player(fakeDice);
+            var dice = new FakeDice(willReturnOnRoll: rollValue);
+            var player = new Player();
+            var yahtzee = new YahtzeeGame();
+
+            var rolls = new[]
+            {
+                player.Roll(dice),
+                player.Roll(dice),
+                player.Roll(dice),
+                player.Roll(dice),
+                player.Roll(dice),
+            };
+
+            Assert.That(yahtzee.CalculateScore(Category.Chance, rolls), Is.EqualTo(expectedScore));
+        }
+
+        [Test]
+        public void PlayerRolls_OnlyTheDesiredValueIsConsideredOnScore()
+        {
+            int expectedScore = 4;
+            var diceValueOne = new FakeDice(willReturnOnRoll: 1);
+            var diceValueTwo = new FakeDice(willReturnOnRoll: 2);
+            var player = new Player();
 
             var yahtzee = new YahtzeeGame();
 
-            var rolls = player.Roll();
+            var firstRoll = player.Roll(diceValueOne);
+            var secondRoll = player.Roll(diceValueTwo);
+            var thirdRoll = player.Roll(diceValueOne);
+            var fourthRoll = player.Roll(diceValueOne);
+            var finalRoll = player.Roll(diceValueOne);
 
-            Assert.That(yahtzee.PlayerScore(rolls), Is.EqualTo(expectedScore));
+            Assert.That(yahtzee.CalculateScore(Category.Ones, firstRoll, secondRoll, thirdRoll, fourthRoll, finalRoll), Is.EqualTo(expectedScore));
         }
+    }
+
+    public enum Category
+    {
+        Chance,
+        Ones
     }
 
     public class FakeDice
     {
-        public FakeDice(int setAllRollsTo)
+        public FakeDice(int willReturnOnRoll)
         {
-            Value = setAllRollsTo;
+            Value = willReturnOnRoll;
         }
 
-        public int Value { get; private set; }
+        public int Value { get; set; }
     }
 
     public class Player
     {
-        private readonly FakeDice fakeDice;
-
-        public Player(FakeDice fakeDice)
+        public int Roll(FakeDice fakeDice)
         {
-            this.fakeDice = fakeDice;
-        }
-
-        public int[] Roll()
-        {
-            return new[] { fakeDice.Value, fakeDice.Value, fakeDice.Value, fakeDice.Value, fakeDice.Value };
+            return  fakeDice.Value;
         }
     }
 
     public class YahtzeeGame
     {
-        public int PlayerScore(int[] rolls)
+        public int CalculateScore(Category category, params int[] rolls)
         {
+            switch (category)
+            {
+                case Category.Ones:
+                    return rolls.Where(r => r == (int)Category.Ones).Sum();
+            }
+
             return rolls.Sum();
         }
     }
